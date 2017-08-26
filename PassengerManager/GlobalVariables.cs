9f2b279afe732e18f1a0cbbeb23410d1cd6f195c
Branden Bean Drake <brandend@uw.edu>
2017-08-26 15:08:41 -0700

    First true commit of Visual Studio Code MVC project.  Currently the project is a combination of the project generated by the "dotnet new mvc" command and codeschool.com's ASP.NET MVC intro project.  There's likely way more here than I need, but I wanted to get it all in sourec control before I started making changes (read: deletions) in a project structure I'm still new to.

---
+++ PassengerManager/GlobalVariables.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using PassengerManager.Models;
+
+namespace PassengerManager
+{
+  public static class GlobalVariables
+  {
+    public static List<Equipment> Equipment { get; set; } = new List<Equipment>();
+  }
+}
